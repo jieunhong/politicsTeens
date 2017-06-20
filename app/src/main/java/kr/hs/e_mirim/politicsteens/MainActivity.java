3182332483f176945cc6da7a850dff6d71c2a499
shimeunji <shimeunji991222@gmail.com>
2017-06-20 09:47:24 +0900

    #2 Navigation Drawer Implementation Success!

--- app/src/main/java/kr/hs/e_mirim/politicsteens/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/politicsteens/MainActivity.java
@@ -1,8 +1,13 @@
 package kr.hs.e_mirim.politicsteens;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -23,8 +28,12 @@ import kr.hs.e_mirim.politicsteens.ContentsFragment;
 import kr.hs.e_mirim.politicsteens.HomeFragment;
 import kr.hs.e_mirim.politicsteens.JustnessFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
+{
 
+    ActionBarDrawerToggle drawerToggle;
+    String[] drawer_str={"내정보","setup","도움말"};
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -43,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nav);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -81,9 +90,41 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        DrawerLayout drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
